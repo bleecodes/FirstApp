7d0c9e6ffdf678278e7bc7f76f6a69dc8b23c618
Brian Lee <writeto.blee@gmail.com>
2016-09-17 22:45:09 -0400

    main,tutorial,act2

---
+++ app/src/main/java/com/example/brian/firstapp/AnotherActivity.java
@@ -0,0 +1,15 @@
+package com.example.brian.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by Brian on 9/17/2016.
+ */
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.another_activity);
+    }
+}
